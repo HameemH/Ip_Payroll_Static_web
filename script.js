d9abb532b207c944c4beab93cee94c54bd7191b9
HameemH <hameemhossain2@gmail.com>
2024-01-26 01:50:33 +0600

    functionality added

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("financebtn").addEventListener("click", ComapanyFinance);
+function ComapanyFinance() 
+{
+    const cashAdd =parseFloat( document.getElementById("cashAdd").value);
+    const loan =parseFloat( document.getElementById("loan").value);
+    const expen =parseFloat( document.getElementById("expen").value);
+    
+    let companyFund=document.getElementById("cp")
+    let employeeSalary=document.getElementById("se")
+    let companyExpense=document.getElementById("ce")
+    let companyloan=document.getElementById("cl")
+    
+    companyFund.innerHTML=Number(companyFund.textContent)+cashAdd;
+    companyExpense.innerHTML=Number(companyExpense.textContent)+expen;
+    companyloan.innerHTML=Number(companyloan.textContent)-loan;
+  
+   
+    
+}
